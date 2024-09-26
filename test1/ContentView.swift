a0530593df707b6957ba6b5e1f21dc3e5cba832f
Ghala <149232664+ghalaghaa@users.noreply.github.com>
2024-09-26 17:06:02 +0300

    Initial Commit

---
+++ test1/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  test1
+//
+//  Created by Ghala Alnemari on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack (spacing: 40) {
+            
+            Image("Screenshot 1446-03-23 at 3.17.09 PM")
+                .resizable()
+          
+               
+        }
+        
+        .padding()
+        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)//        .padding()
+//        .frame(maxWidth: .infinity)
+//        .foregroundColor(.white)
+//        .background(Color.blue)
+//        .cornerRadius(15)
+//        .padding()
+  }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ test1/test1App.swift
@@ -0,0 +1,17 @@
+//
+//  test1App.swift
+//  test1
+//
+//  Created by Ghala Alnemari on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
